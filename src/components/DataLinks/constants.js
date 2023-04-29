e7f5c1e162522759fe494816e7021a8c45721a85
CryptoFallen <richardtom1203@protonmail.com>
2023-04-29 16:02:00 +0100

    Update to Footer Section

---
+++ src/components/DataLinks/constants.js
@@ -0,0 +1,25 @@
+export const Company = {
+  "About Us": "/about",
+  "Brand Assets": "/brand",
+  "Contact Us": "/contact",
+  Careers: "/careers",
+  "Terms of Service": "/terms",
+  "Bug Bounty": "/bug-bounty",
+};
+
+export const Community = {
+  "API Documentation": "/api-docs",
+  "Knowledge Base": "/knowledge-base",
+  "Network Status": "/network-status",
+  Newsletters: "/newsletters",
+  "Disqus Comments": "/disqus-comments",
+};
+
+export const ProductsAndServices = {
+  Advertise: "/advertise",
+  "Explorer-as-a-Service (EaaS)": "/explorer-as-a-service",
+  "API Plans": "/api-plans",
+  "Priority Support": "/priority-support",
+  Blockscan: "/blockscan",
+  "Blockscan Chat": "/blockscan-chat",
+};
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,25 +1,27 @@
 import { EthereumIcon } from "../icons";
+import { Company, Community, ProductsAndServices } from "./DataLinks/constants";
 
 const Footer = () => {
   const onTop = () => {
     window.scrollTo(0, 0);
   };
   return (
-    <footer className="mt-10 bg-primaryBgLight py-10 dark:bg-primaryBgDark">
-      <div className="m-auto max-w-7xl px-5">
+    <footer className="mt-10 bg-primaryBgLight pb-0 pt-10 dark:bg-primaryBgDark">
+      {/* {max-w-7xl } */}
+      <div className="m-auto px-5">
         <div className="border-b border-b-secondaryBgLight">
-          <div className="mb-3 flex flex-wrap justify-between px-3 md:flex-nowrap">
+          <div className="mb-3 flex flex-wrap justify-between md:flex-nowrap">
             <div className="flex w-24 justify-between">
               <a
                 href="https://github.com/r-tom90"
                 target="blank"
-                className="github-link flex items-center gap-2 rounded-full bg-primaryBgLight p-1"
+                className="social-link flex items-center gap-2 rounded-full bg-primaryBgLight p-1 hover:bg-activeLight"
               >
                 <svg
                   height="20"
                   width="20"
                   viewBox="0 0 16 16"
-                  className="rounded-full bg-primaryBgLight fill-primaryBgDark"
+                  className="rounded-full bg-primaryBgLight fill-primaryBgDark hover:bg-activeLight"
                 >
                   <path d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"></path>
                 </svg>
@@ -27,7 +29,7 @@ const Footer = () => {
               <a
                 href="https://twitter.com/CryptoFallen"
                 target="blank"
-                className="twitter-link flex items-center gap-2 rounded-full bg-primaryBgLight p-1"
+                className="social-link flex items-center gap-2 rounded-full bg-primaryBgLight p-1 hover:bg-activeLight"
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -36,7 +38,7 @@ const Footer = () => {
                   height="20"
                   width="20"
                   viewBox="0 0 30 30"
-                  className="rounded-full bg-primaryBgLight fill-primaryBgDark"
+                  className="rounded-full bg-primaryBgLight fill-primaryBgDark hover:bg-activeLight"
                 >
                   <path d="M28,6.937c-0.957,0.425-1.985,0.711-3.064,0.84c1.102-0.66,1.947-1.705,2.345-2.951c-1.03,0.611-2.172,1.055-3.388,1.295 c-0.973-1.037-2.359-1.685-3.893-1.685c-2.946,0-5.334,2.389-5.334,5.334c0,0.418,0.048,0.826,0.138,1.215 c-4.433-0.222-8.363-2.346-10.995-5.574C3.351,6.199,3.088,7.115,3.088,8.094c0,1.85,0.941,3.483,2.372,4.439 c-0.874-0.028-1.697-0.268-2.416-0.667c0,0.023,0,0.044,0,0.067c0,2.585,1.838,4.741,4.279,5.23 c-0.447,0.122-0.919,0.187-1.406,0.187c-0.343,0-0.678-0.034-1.003-0.095c0.679,2.119,2.649,3.662,4.983,3.705 c-1.825,1.431-4.125,2.284-6.625,2.284c-0.43,0-0.855-0.025-1.273-0.075c2.361,1.513,5.164,2.396,8.177,2.396 c9.812,0,15.176-8.128,15.176-15.177c0-0.231-0.005-0.461-0.015-0.69C26.38,8.945,27.285,8.006,28,6.937z"></path>
                 </svg>
@@ -44,7 +46,7 @@ const Footer = () => {
               <a
                 href="https://www.linkedin.com/in/richard-tom-81b0956b"
                 target="blank"
-                className="twitter-link flex items-center gap-2 rounded-full bg-primaryBgLight p-1"
+                className="social-link flex items-center gap-2 rounded-full bg-primaryBgLight p-1 hover:bg-activeLight"
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -53,7 +55,7 @@ const Footer = () => {
                   height="20"
                   width="20"
                   viewBox="0 0 30 30"
-                  className="rounded-full bg-primaryBgLight fill-primaryBgDark"
+                  className="rounded-full bg-primaryBgLight fill-primaryBgDark hover:bg-activeLight"
                 >
                   <path d="M9,25H4V10h5V25z M6.501,8C5.118,8,4,6.879,4,5.499S5.12,3,6.501,3C7.879,3,9,4.121,9,5.499C9,6.879,7.879,8,6.501,8z M27,25h-4.807v-7.3c0-1.741-0.033-3.98-2.499-3.98c-2.503,0-2.888,1.896-2.888,3.854V25H12V9.989h4.614v2.051h0.065 c0.642-1.18,2.211-2.424,4.551-2.424c4.87,0,5.77,3.109,5.77,7.151C27,16.767,27,25,27,25z"></path>
                 </svg>
@@ -69,35 +71,84 @@ const Footer = () => {
           </div>
         </div>
 
-        <div className="mt-5">
-          <div className="h-20 px-3 md:h-16">
-            <div className="flex">
-              <EthereumIcon />
-              <h4 className="ml-1 inline-flex items-center font-normal">
-                Powered by Alchemy & Ethers SDK
-              </h4>
-            </div>
-            <p className="mt-2 text-sm text-secondaryBgDark dark:text-secondaryBgLight">
-              Etherscan is a Block Explorer and Analytics Platform for Ethereum,
-              a decentralized smart contracts platform.
-            </p>
-          </div>
+        <div className="mt-5 block lg:flex">
+          <div>
+            <div className="lg:h-16 lg:min-h-[300px]">
+              <div className="flex">
+                <EthereumIcon />
+                <h4 className="ml-1 inline-flex items-center font-normal">
+                  Powered by Alchemy & Ethers SDK
+                </h4>
+              </div>
+              <div>
+                <p className="mt-2 w-full text-xs text-secondaryBgDark dark:text-secondaryBgLight lg:w-[300px]">
+                  <span className="font-semibold text-yellow-300">
+                    RichEtherScan
+                  </span>{" "}
+                  is a user-friendly Ethereum block explorer and analytics
+                  platform powered by Alchemy & Ethers SDK, enabling developers
+                  to explore, verify and analyze blockchain data.
+                </p>
+              </div>
 
-          <picture className="hidden md:block">
-            <img
-              width="250"
-              src="/public/images/map.png"
-              alt="map"
-              className="block opacity-50 dark:hidden"
-            />
-            <img
-              height="20"
-              width="250"
-              src="/public/images/map-light.png"
-              alt="map"
-              className="hidden opacity-50 dark:block"
-            />
-          </picture>
+              <picture className="hidden lg:block">
+                <img
+                  width="300"
+                  src="../../public/images/map.png"
+                  alt="map"
+                  className="block opacity-50 dark:hidden"
+                />
+                <img
+                  width="300"
+                  src="../../public/images/map-light.png"
+                  alt="map"
+                  className="hidden opacity-50 dark:block"
+                />
+              </picture>
+            </div>
+          </div>
+          <section className="mx-0 my-5 flex w-full flex-row lg:ml-20 lg:mt-0">
+            <div className="w-full px-0 sm:w-1/3 lg:px-3">
+              <h3 className="font-medium">Company</h3>
+              <ul>
+                {Object.keys(Company).map((title) => (
+                  <li className="my-4 text-xs" key={title}>
+                    <a href={Company[title]}>{title}</a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className="w-full px-0 sm:w-1/3 lg:px-3">
+              <h3 className="font-medium">Community</h3>
+              <ul>
+                {Object.keys(Community).map((title) => (
+                  <li className="my-4 text-xs" key={title}>
+                    <a href={Community[title]}>{title}</a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className="w-full px-0 sm:w-1/3 lg:px-3">
+              <h3 className="font-medium">Products & Services</h3>
+              <ul>
+                {Object.keys(ProductsAndServices).map((title) => (
+                  <li className="my-4 text-xs" key={title}>
+                    <a href={ProductsAndServices[title]}>{title}</a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </section>
+        </div>
+        <div className="flex flex-col justify-between text-xs text-primaryTextLight dark:text-primaryTextDark md:flex-row">
+          <p className="my-2 ">RichEtherScan &#169; 2023</p>
+          <p className="my-2 ">
+            Donations:
+            <span className="mx-1 text-activeLight dark:text-activeDark">
+              0xB4d3cC81c3af96fBCeBEB7C8Dc92CE5025d30348
+            </span>
+            ❤️
+          </p>
         </div>
       </div>
     </footer>
